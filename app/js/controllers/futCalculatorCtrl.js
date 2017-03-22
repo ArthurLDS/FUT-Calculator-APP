e315d5872ec085882307e9877624237a95103710
ArthurLDS <arthurlds73@gmail.com>
2017-03-22 20:34:27 -0300

    refactor(futCalulatorCtrl): refatorar método calculateTrade.

--- app/js/controllers/futCalculatorCtrl.js
+++ app/js/controllers/futCalculatorCtrl.js
@@ -4,17 +4,14 @@ angular.module("futCalculatorApp").controller("futCalculatorCtrl", function($sco
   $scope.$tradeList = $localStorage.tradeList ? $localStorage.tradeList : [];
 
   $scope.calculate = function(trade){
-
-     $scope.trade.profitBid = calculateProfit(trade.purchasePrice, trade.sellBidPrice) * trade.numCardsSolds;
-     $scope.trade.eaTaxBid = discont(trade.sellBidPrice, 5) * trade.numCardsSolds;
-     $scope.trade.isBidProfit = isBidProfit(trade);
-
-     $scope.trade.profitBuyNow = calculateProfit(trade.purchasePrice, trade.sellBuyNowPrice) * trade.numCardsSolds;
-     $scope.trade.eaTaxBuyNow = discont(trade.sellBuyNowPrice, 5) * trade.numCardsSolds;
-     $scope.trade.isBuyNowProfit = isBuyNowProfit(trade);
+    $scope.trade = calculateTrade(trade);
   };
 
   $scope.saveTrade = function(trade){
+    
+    if(Object.keys(trade).length <= 5)
+      trade = calculateTrade(trade);
+
     if(!trade.name)
       trade.name = "Unnamed trade";
     $scope.$tradeList.push(trade);
@@ -26,10 +23,22 @@ angular.module("futCalculatorApp").controller("futCalculatorCtrl", function($sco
   $scope.deleteTrade = function(trade){
     $scope.$tradeList = $scope.$tradeList.filter(t => t!=trade);
     $localStorage.tradeList = $scope.$tradeList;
-  }
+  };
 
   $scope.isEmptyObject = function(obj){
     return Object.keys(obj).length === 0;
+  };
+
+  let calculateTrade = function(trade){
+     trade.profitBid = calculateProfit(trade.purchasePrice, trade.sellBidPrice) * trade.numCardsSolds;
+     trade.eaTaxBid = discont(trade.sellBidPrice, 5) * trade.numCardsSolds;
+     trade.isBidProfit = isBidProfit(trade);
+
+     trade.profitBuyNow = calculateProfit(trade.purchasePrice, trade.sellBuyNowPrice) * trade.numCardsSolds;
+     trade.eaTaxBuyNow = discont(trade.sellBuyNowPrice, 5) * trade.numCardsSolds;
+     trade.isBuyNowProfit = isBuyNowProfit(trade);
+
+     return trade;
   };
 
   let calculateProfit = function(purchasePrice, sellBidPrice){
@@ -39,7 +48,7 @@ angular.module("futCalculatorApp").controller("futCalculatorCtrl", function($sco
 
   let discont = function(value, percent){
     return Math.round((value * percent) / 100);
-  }
+  };
 
   let isBidProfit = function(trade){
     return trade.profitBid > 0;
