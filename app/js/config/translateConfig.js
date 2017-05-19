c92c69b079af525fccbb12abbbbd8f3a2664e9ab
ArthurLDS <arthurlds73@gmail.com>
2017-05-19 13:28:48 -0300

    feat(translateConfig): implementar traduções das opções de trade.

--- app/js/config/translateConfig.js
+++ app/js/config/translateConfig.js
@@ -18,9 +18,11 @@ angular.module("futCalculatorApp").config(function($translateProvider){
     BTN_SAVE_TRADE: "Save Trade",
     //Menu Itens
     TRADE_PANEL: "Trade painel",
-    ABOUT_DEVELOPER: "About the delveloper",
+    ABOUT_DEVELOPER: "About the developer",
+    ABOUT_DEVELOPER_SHORT: "About the dev",
     ABOUT_THE_APP: "About the APP",
     DOWNLOAD_PLAY_STORE: "Download the APP on Play Store",
+    DOWNLOAD_PLAY_STORE_SHORT: "Download the APP",
     COLLABORATE_GITHUB: "Collaborate with us on GitHub",
     //Form validacions
     ALERT_FORM_TRADE_INVALID: "There are blank fields! Please fill them.",
@@ -51,8 +53,10 @@ angular.module("futCalculatorApp").config(function($translateProvider){
     //Menu Itens
     TRADE_PANEL: "Painel de Trade",
     ABOUT_DEVELOPER: "Sobre o desenvolvedor",
+    ABOUT_DEVELOPER_SHORT: "Sobre o dev",
     ABOUT_THE_APP: "Sobre o APP",
     DOWNLOAD_PLAY_STORE: "Baixe o APP na Play Store",
+    DOWNLOAD_PLAY_STORE_SHORT: "Baixe o APP",
     COLLABORATE_GITHUB: "Colabore conosco no GitHub",
     //Form validacions
     ALERT_FORM_TRADE_INVALID: "Há campos obrigatórios em branco.",
